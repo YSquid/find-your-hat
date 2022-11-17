4c5d4e44a3aeb4d54baa6bb0e1158c5fbaeb1ab5
Ysquid <ahk648@gmail.com>
2022-11-17 15:08:27 -0600

    have the isInbounds method working and tested

--- main.js
+++ main.js
@@ -7,43 +7,61 @@ const pathCharacter = "*";
 
 class Field {
   constructor(field) {
-    this.field = field
+    this.field = field;
+    this.locationY = 0;
+    this.locationX = 0;
   }
 
   print() {
-    const displayString = this.field.map((row) => {
-      return row.join('')
-    }).join('\n')
-    console.log(displayString)
+    const displayString = this.field
+      .map((row) => {
+        return row.join("");
+      })
+      .join("\n");
+    console.log(displayString);
   }
 
   askQuestion() {
-    const move = prompt('Which Way?').toUpperCase()
-    switch(move) {
-      case 'U':
+    const move = prompt("Which Way?").toUpperCase();
+    switch (move) {
+      case "U":
         this.locationY -= 1;
         break;
-      case 'D':
+      case "D":
         this.locationY += 1;
         break;
-      case 'L':
+      case "L":
         this.locationX -= 1;
         break;
-      case 'R':
+      case "R":
         this.locationX += 1;
         break;
       default:
-        console.log('Enter valid direction: U, D, L, R')
+        console.log("Enter valid direction: U, D, L, R");
     }
   }
 
-
+  isInbounds() {
+    if (this.locationY < 0 || this.locationX < 0) {
+      return false;
+    } else if (
+      this.locationY > this.field.length - 1 ||
+      this.locationX > this.field[0].length - 1
+    ) {
+      return false;
+    } else {
+      return true
+    }
+  }
 }
-
 const myField = new Field([
   ["*", "░", "O"],
   ["░", "O", "░"],
   ["░", "^", "░"],
-])
+]);
 
-myField.print()
+myField.print();
+myField.askQuestion();
+console.log(myField.locationY);
+console.log(myField.locationX);
+console.log(myField.isInbounds())
