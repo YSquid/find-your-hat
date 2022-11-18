b241759be0bef411b68efee682dc86900706fe3e
Ysquid <ahk648@gmail.com>
2022-11-18 10:30:05 -0600

    started validateField function

---
+++ validate-field-plan.md
@@ -0,0 +1,7 @@
+Pass the field to the validate field and it needs to basically run the game
+
+Create a variable valid field set to null at start
+
+
+while valid field is null run a while loop of trying the field
+
---
+++ validateField.js
@@ -0,0 +1,23 @@
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
+
+function validateField(field) {
+
+  const location = field[0][0];
+  const validField = null
+  const visitedNodes = [
+
+  ]
+
+//   while (validField === null) {
+        
+//   }
+  console.log(field)
+  console.log(field[0][0])
+}
+
+console.log(validateField([[pathCharacter, hole], [fieldCharacter, hat]]));
+
+
